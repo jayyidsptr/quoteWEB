cc5d846627ea6165863cb91f9bf55317eababf6a
Jayyid <45623599+jayyidsptr@users.noreply.github.com>
2024-05-15 01:09:16 +0800

    second upload

--- script.js
+++ script.js
@@ -1,32 +1,16 @@
 function fetchQuote() {
-    const category = document.getElementById('categorySelect').value;
-    const display = document.getElementById('quoteDisplay');
+    const category = $('#categorySelect').val();
+    const display = $('#quoteDisplay');
 
     // Membuat permintaan ke server untuk mendapatkan kutipan
-    fetch(`https://quotes-api-beta.vercel.app/api/quotes/${category}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
+    $.ajax({
+        url: `https://quotes-api-beta.vercel.app/api/quotes/${category}`,
+        dataType: 'json',
+        error: function (xhr, status, error) {
+            display.text(error.message);
         },
-    })
-        .then(response => {
-            if (!response.ok) {
-                if (response.status === 404) {
-                    throw new Error('Kategori tidak ditemukan.');
-                } else {
-                    throw new Error('Terjadi kesalahan pada server.');
-                }
-            }
-            return response.json();
-        })
-        .then(data => {
-            display.textContent = data.quote;
-        })
-        .catch(error => {
-            if (error.message === 'Kategori tidak ditemukan.') {
-                display.textContent = 'Tidak ada kutipan ditemukan untuk kategori ini.';
-            } else {
-                display.textContent = error.message;
-            }
-        });
+        success: function (data) {
+            display.text(data.quote);
+        }
+    });
 }
