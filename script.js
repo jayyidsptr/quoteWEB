6948c2508e2333945554bc861f54eefb04bf7e14
Jayyid <45623599+jayyidsptr@users.noreply.github.com>
2024-05-15 00:16:26 +0800

    first upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function fetchQuote() {
+    const category = document.getElementById('categorySelect').value;
+    const display = document.getElementById('quoteDisplay');
+
+    // Membuat permintaan ke server untuk mendapatkan kutipan
+    fetch(`https://quotes-api-beta.vercel.app/api/quotes/${category}`, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then(response => {
+            if (!response.ok) {
+                if (response.status === 404) {
+                    throw new Error('Kategori tidak ditemukan.');
+                } else {
+                    throw new Error('Terjadi kesalahan pada server.');
+                }
+            }
+            return response.json();
+        })
+        .then(data => {
+            display.textContent = data.quote;
+        })
+        .catch(error => {
+            if (error.message === 'Kategori tidak ditemukan.') {
+                display.textContent = 'Tidak ada kutipan ditemukan untuk kategori ini.';
+            } else if (error.message === 'Terjadi kesalahan pada server.') {
+                display.textContent = 'Terjadi kesalahan saat mengambil kutipan.';
+            } else {
+                display.textContent = error.message;
+            }
+        });
+}
